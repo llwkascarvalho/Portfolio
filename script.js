dac2023084e132fd93e2f8a1ed11d7f09aceb743
Lwkas Carvalho <lwkascarvalho1@gmail.com>
2023-11-01 01:52:44 -0300

    create card

--- script.js
+++ script.js
@@ -8,38 +8,3 @@ document.addEventListener("DOMContentLoaded", function() {
       });
     });
   });
-
-
-// linha do tempo 
-
-document.getElementById("frontEndBtn").addEventListener("click", function () {
-  filterCursos("Front-end");
-});
-
-document.getElementById("backEndBtn").addEventListener("click", function () {
-  filterCursos("Back-end");
-});
-
-document.getElementById("desenvolvimentoPessoalBtn").addEventListener("click", function () {
-  filterCursos("Desenvolvimento Pessoal");
-});
-
-document.getElementById("empreendedorismoBtn").addEventListener("click", function () {
-  filterCursos("Empreendedorismo");
-});
-
-
-
-// Função para filtrar os cursos
-function filterCursos(categoria) {
-  const cursos = document.querySelectorAll(".curso");
-  cursos.forEach((curso) => {
-    const categoriaCurso = curso.getAttribute("data-categoria");
-
-    if (categoria === "Todos" || categoria === categoriaCurso) {
-      curso.style.display = "block";
-    } else {
-      curso.style.display = "none";
-    }
-  });
-}
